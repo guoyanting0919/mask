d86d05a66a157bfdc63ee017132f25a53fa8dfbc
YT <guoyanting0919@gmail.com>
2020-02-09 21:11:39 +0800

    user site

---
+++ src/components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="loader">
+    <div class="loading"></div>
+    <span class="loadingTxt">LOADING...</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
